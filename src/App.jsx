60f637cf62c98001ddfdec660d74f0bf1582546d
Melissa Mondot <119279111+mondotDev@users.noreply.github.com>
2025-04-23 14:51:12 -0700

    Css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
-import SchoolScheduleTimer from './components/SchoolScheduleTimer';
+import React from "react";
+import SchoolScheduleTimer from "./components/SchoolScheduleTimer";
 
 function App() {
-  return <SchoolScheduleTimer />;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <SchoolScheduleTimer />
+    </div>
+  );
 }
 
 export default App;
